293cc06945690c4a4e86e330d3f1202c1b2d12cd
Daniel Lozano <d.lozano343@uniandes.edu.co>
2015-10-21 21:41:17 -0500

    actualizado

--- lectura_grafica.py
+++ lectura_grafica.py
@@ -8,20 +8,22 @@ import sys
 lectura=str(sys.argv[1])
 archivo=np.loadtxt(open(lectura,"r"))
 
-x=archivo[:,0]
-y=archivo[:,1]
+x=archivo[:,3]
+y=archivo[:,5]
+
 
 X="$ "+ str(sys.argv[-2]) + " $"
 Y="$ "+ str(sys.argv[-1]) + " $"
 
-#x=np.linspace(0,10,1000)
-#y=np.sin(x)
+x=np.linspace(0,10,1000)
+y=np.sin(x)
 
-plt.plot(x,y)
+plt.scatter(x-y,x)
 plt.xlabel(X,size=20)
 plt.ylabel(Y,size=20)
 plt.gca().invert_xaxis()
 plt.show()
+plt.savefig("magnitud_color.png")
 plt.close()
 
 
